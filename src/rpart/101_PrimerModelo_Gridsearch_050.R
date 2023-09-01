194ed227d3660c8a77980fbefd122f3436ec39f3
vickyfsa <fernandez.suarez.victoria@gmail.com>
2023-09-01 20:58:29 +0000

    Gridsearch_100

--- src/rpart/101_PrimerModelo_Gridsearch_050.R
+++ src/rpart/101_PrimerModelo_Gridsearch_050.R
@@ -24,10 +24,10 @@ modelo <- rpart(
         formula = "clase_ternaria ~ .",
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
-        cp = -0.705406284124855, # esto significa no limitar la complejidad de los splits
-        minsplit = 2762, # minima cantidad de registros para que se haga el split
-        minbucket = 9, # tamaño minimo de una hoja
-        maxdepth = 16
+        cp = -0.999799166182308, # esto significa no limitar la complejidad de los splits
+        minsplit = 1790, # minima cantidad de registros para que se haga el split
+        minbucket = 885, # tamaño minimo de una hoja
+        maxdepth = 14
 ) # profundidad maxima del arbol
 
 
@@ -63,7 +63,7 @@ dir.create("./exp/KA2001")
 
 # solo los campos para Kaggle
 fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_Gridsearch_050.csv",
+        file = "./exp/KA2001/K101_Gridsearch_100.csv",
         sep = ","
 )
 print("Done!")
