5405f42e79a36ffeaa69e0fd4cb7c99ba90e072a
vickyfsa <fernandez.suarez.victoria@gmail.com>
2023-09-08 23:40:26 +0000

    611_01

--- src/workflow-inicial/611_01_CA_reparar_dataset.r
+++ src/workflow-inicial/611_01_CA_reparar_dataset.r
@@ -10,7 +10,7 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "CA6110"
+PARAM$experimento <- "CA6111"
 PARAM$dataset <- "./datasets/competencia_2023.csv.gz"
 
 # valores posibles
