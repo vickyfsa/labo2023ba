72c7634290cd746a82e6c3425f5bc2c5dbfdd008
vickyfsa <fernandez.suarez.victoria@gmail.com>
2023-09-17 02:13:53 +0000

    631

--- src/workflow-inicial/631_000_FE_historia.r
+++ src/workflow-inicial/631_000_FE_historia.r
@@ -49,7 +49,7 @@ PARAM$RandomForest$num.trees <- 20
 PARAM$RandomForest$max.depth <- 4
 PARAM$RandomForest$min.node.size <- 1000
 PARAM$RandomForest$mtry <- 40
-PARAM$RandomForest$semilla <- 102191 # cambiar por la propia semilla
+PARAM$RandomForest$semilla <- 703127 # cambiar por la propia semilla
 
 
 # varia de 0.0 a 2.0, si es 0.0 NO se activan
@@ -57,7 +57,7 @@ PARAM$CanaritosAsesinos$ratio <- 0.0
 # desvios estandar de la media, para el cutoff
 PARAM$CanaritosAsesinos$desvios <- 4.0
 # cambiar por la propia semilla
-PARAM$CanaritosAsesinos$semilla <- 200177
+PARAM$CanaritosAsesinos$semilla <- 821501
 
 PARAM$home <- "~/buckets/b1/"
 # FIN Parametros del script
