e6c76f93b5ddf3b0f5e1187ab07e035d4d3db24b
vickyfsa <fernandez.suarez.victoria@gmail.com>
2023-09-27 22:15:11 -0300

    631_021

---
+++ src/workflow-inicial/601_021_RUN_correr_workflow.r
@@ -0,0 +1,21 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/labo2023ba/src/workflow-inicial/611_000_CA_reparar_dataset.r")
+source("~/labo2023ba/src/workflow-inicial/621_000_DR_corregir_drifting.r")
+source("~/labo2023ba/src/workflow-inicial/631_021_FE_historia.r")
+source("~/labo2023ba/src/workflow-inicial/641_021_TS_training_strategy.r")
+
+# ultimos pasos, muy lentos
+source("~/labo2023ba/src/workflow-inicial/651_021_HT_lightgbm.r")
+source("~/labo2023ba/src/workflow-inicial/661_021_ZZ_final.r")
+print("Done RUN correr workflow")
